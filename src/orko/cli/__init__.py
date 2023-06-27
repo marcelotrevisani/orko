2a2d5c0b68a15fbe9b737bed75721656543f0106
Marcelo Trevisani <marceloduartetrevisani@gmail.com>
2023-06-27 06:59:05 +0100

    Add cli to create conda env form pyproject.toml

--- src/orko/cli/__init__.py
+++ src/orko/cli/__init__.py
@@ -1,4 +1,7 @@
 import rich_click as click
+from orko.core.env import create_conda_env
+from orko.core.process import get_deps
+from orko.core.process import load_pyproject
 
 
 @click.command(name="create")
@@ -23,5 +26,28 @@ import rich_click as click
     multiple=True,
     help="Tags that orko will look for when creating the environment",
 )
-def cli_create_env():
-    pass
+@click.option(
+    "--conda-bin",
+    default="conda",
+    help="Conda executable path file, it also accepts mamba/micromamba.",
+    required=False,
+)
+@click.option(
+    "--extra-args-conda",
+    help="Extra arguments to be passed to the `conda create`.",
+    type=str,
+)
+def cli_create_env(
+    pyproject_file, name, channels, add_deps, verbose, tags, conda_bin, extra_args_conda
+):
+    # TODO: Need to add the verbose action which is going to control the logging level
+    add_deps = add_deps or []
+    pyproject = load_pyproject(pyproject_file)
+    req_deps, opt_deps = get_deps(pyproject, tags)
+    create_conda_env(
+        name,
+        conda_bin=conda_bin,
+        conda_options=extra_args_conda,
+        list_deps=add_deps + req_deps + opt_deps,
+        conda_channels=channels,
+    )
